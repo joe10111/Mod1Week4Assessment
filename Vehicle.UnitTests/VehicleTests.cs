d4c57ad4644e64026d19717f72083bf9ca73fe1d
Joe Centeno <centeno.joe@gmail.com>
2023-04-13 09:23:25 -0700

    Turned in Test with updated code

--- Vehicle.UnitTests/VehicleTests.cs
+++ Vehicle.UnitTests/VehicleTests.cs
@@ -2,7 +2,8 @@ namespace Vehicle.UnitTests
 {
     public class VehicleTests
     {
-        [Fact]
+         // A test to check if all properties initialize correctly in the Vehicle constructor 
+        [Fact] 
         public void WhenCreated_HasCorrectProperties()
         {
             Vehicle megansCar = new Vehicle(4, "Green");
@@ -12,17 +13,40 @@ namespace Vehicle.UnitTests
             Assert.Equal(0, megansCar.MilesDriven);
         }
 
-        // This test needs some work!
+         // A test to check if the correct summary string is generated and returned
         [Fact]
         public void Summary_ReturnsInfoAboutVehicle()
         {
             Vehicle andysBike = new Vehicle(2, "Blue");
+             // Also updated the expected to reflect string formatt of summary()
+            var expected = "This Blue vehicle has 2 wheels, and has driven 0 miles.";
 
-            var expected = "This Blue vehicle has 2 wheels and has driven 0 miles";
-
-
+            // Aded assert bellow checking expected vs summary call on andysBike
+            Assert.Equal(expected, andysBike.Sumary());
         }
 
+         //A test to check if the drive method is correctly incrementing the MilesDriven property
+        [Fact] 
+        public void Drive()
+        {
+            Vehicle andysBike = new Vehicle(2, "Blue");
+
+            andysBike.Drive();
+
+            var expected = 5;
+            Assert.Equal(expected, andysBike.MilesDriven);
+        }
+
+         // A test to check if the paint method is working correctly with re-assigning the Color property. 
+        [Fact] 
+        public void Paint()
+        {
+            Vehicle andysBike = new Vehicle(2, "Blue");
+            andysBike.Paint("Red");
+
+            var expected = "Red";
+            Assert.Equal(expected, andysBike.Color);
+        }
         // Add more tests here!
     }
 }
